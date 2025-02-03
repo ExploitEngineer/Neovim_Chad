c8fcfc2c480626b37039886118d5bb224ffc2186
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-03 18:24:35 +0500

    Initial commit Add files for neovim setup

---
+++ lua/plugins/init.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim.tree/nvim-tree.lua",
+    opts = {
+      git = { enable = true},
+    }, 
+  },
+  {
+    "smoka7/hop.nvim",
+    opts = {
+      multi_windows = true,
+      keys = "htnsueoaidgcrlypmbkjvx",
+      uppercase_labels = true,
+    },
+    keys = {
+      {
+        "<leader>fj",
+        function() 
+          require("hop").hint_words()
+        end,
+        mode = { "n", "x", "o" },
+      },
+    },
+  }
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}

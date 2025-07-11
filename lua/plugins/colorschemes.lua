c9e8f4112914cd0999c783b9fc50196fc0cda916
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-11 06:03:39 +0500

    feat: add lazyvim setup
    Add lazyvim with all important plugins settings, configs and also
    wakatime is setup

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,34 @@
+return {
+  -- TokyoNight (blue/purple vibes)
+  { "folke/tokyonight.nvim", lazy = true, priority = 1000 },
+
+  -- Catppuccin (soft pastel, very popular)
+  { "catppuccin/nvim", name = "catppuccin", lazy = true, priority = 1000 },
+
+  -- Gruvbox (warm, classic)
+  { "ellisonleao/gruvbox.nvim", lazy = true, priority = 1000 },
+
+  -- One Dark (Atom's classic)
+  { "navarasu/onedark.nvim", lazy = true, priority = 1000 },
+
+  -- Rose Pine (calm, elegant)
+  { "rose-pine/neovim", name = "rose-pine", lazy = true, priority = 1000 },
+
+  -- Kanagawa (inspired by Japanese art)
+  { "rebelot/kanagawa.nvim", lazy = true, priority = 1000 },
+
+  -- Nord (cool, arctic)
+  { "shaunsingh/nord.nvim", lazy = true, priority = 1000 },
+
+  -- Dracula (purple, high contrast)
+  { "Mofiqul/dracula.nvim", lazy = true, priority = 1000 },
+
+  -- Everforest (nature-based)
+  { "sainnhe/everforest", lazy = true, priority = 1000 },
+
+  -- Nightfox (fox-themed variants: nordfox, duskfox, etc.)
+  { "EdenEast/nightfox.nvim", lazy = true, priority = 1000 },
+
+  -- Oxocarbon (Carbon Design by IBM)
+  { "nyoom-engineering/oxocarbon.nvim", lazy = true, priority = 1000 },
+}

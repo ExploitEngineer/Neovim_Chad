b990f036a94dab868cdc83202d4924b286ea3335
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-12 08:41:07 +0500

    remove: material.icons.nvim plugin

--- lua/plugins/material-icons.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "DaikyXendo/nvim-material-icon",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("nvim-material-icon").setup()
-  end,
-}
